5849678a0036bc25177bdc13e9a4aad8a445d22e
DaiSmallEast <970439131@qq.com>
2018-02-22 00:06:21 +0800

    add something to SufeHelperAPP

--- app/src/main/java/com/example/sufehelperapp/MyActivity_Historical_Task.java
+++ app/src/main/java/com/example/sufehelperapp/MyActivity_Historical_Task.java
@@ -1,46 +1,37 @@
 package com.example.sufehelperapp;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.SpinnerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.sufehelperapp.fragmentAdapter;
-import com.example.sufehelperapp.viewAdapter;
+public class MyActivity_Historical_Task extends AppCompatActivity {
 
-public class MyActivity_Mytask extends AppCompatActivity {
+    private TabLayout tab_title2;
+    private ViewPager vp_pager2;
 
-    private TabLayout tab_title;
-    private ViewPager vp_pager;
+    private List<String> list_title2;    //tab名称列表
+    private List<View> listViews2;
+    private List<Fragment> list_fragment2;
 
-    private List<String> list_title;    //tab名称列表
-    private List<View> listViews;
-    private List<Fragment> list_fragment;
+    private View recievedtaskView;  //定义接受任务界面
+    private View republishtaskView;  //定义发布任务界面
 
-    private View currenttaskView;  //定义当前任务界面
-    private View historicaltaskView;  //定义历史任务界面
+    private viewAdapter_history vAdapter; //定义以view为切换的Adapter
+    private fragmentAdapter_history fAdapter;  //定义以fragment为切换的Adapter
 
-    private viewAdapter vAdapter; //定义以view为切换的Adapter
-    private fragmentAdapter fAdapter;  //定义以fragment为切换的Adapter
-
-    private MyActivity_mytask_currenttask cFragment;
-    private MyActivity_mytask_historicaltask hFragment;
+    private MyActivity_History_Recieved cFragment;
+    private MyActivity_History_Republish hFragment;
     private ImageButton imgbtn1;
 
     private int[] tabImg;
@@ -48,7 +39,7 @@ public class MyActivity_Mytask extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_mytask);
+        setContentView(R.layout.activity_my_historical_task);
 
         ActionBar actionBar = getSupportActionBar();
         if(actionBar != null) {
@@ -58,15 +49,7 @@ public class MyActivity_Mytask extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MyActivity_Mytask.this, My_HomeActivity.class);
-                startActivity(intent);
-            }
-        });
-        Button button2 = (Button) findViewById(R.id.title_edit);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MyActivity_Mytask.this, MyActivity_Historical_Task.class);
+                Intent intent = new Intent(MyActivity_Historical_Task.this, MyActivity_Mytask.class);
                 startActivity(intent);
             }
         });
@@ -95,8 +78,8 @@ public class MyActivity_Mytask extends AppCompatActivity {
      * 初始化控件
      */
     private void initControls() {
-        tab_title = (TabLayout) findViewById(R.id.tab_title);
-        vp_pager = (ViewPager) findViewById(R.id.vp_pager);
+        tab_title2 = (TabLayout) findViewById(R.id.tab_title2);
+        vp_pager2 = (ViewPager) findViewById(R.id.vp_pager2);
 
         //为tabLayout上的图标赋值
         tabImg = new int[]{R.drawable.ic_explore,R.drawable.ic_explore};
@@ -107,35 +90,35 @@ public class MyActivity_Mytask extends AppCompatActivity {
 
 
     private void viewChange() {
-        listViews = new ArrayList<>();
+        listViews2 = new ArrayList<>();
         LayoutInflater mInflater = getLayoutInflater();
 
-        currenttaskView = mInflater.inflate(R.layout.activity_my_mytask_currenttask,null);
-        historicaltaskView = mInflater.inflate(R.layout.activity_my_mytask_historicaltask,null);
-        listViews.add(currenttaskView);
-        listViews.add(historicaltaskView);
+        recievedtaskView = mInflater.inflate(R.layout.activity_my_history_recieved,null);
+        republishtaskView = mInflater.inflate(R.layout.activity_my_history_republish,null);
+        listViews2.add(recievedtaskView);
+        listViews2.add(republishtaskView);
 
-        list_title = new ArrayList<>();
-        list_title.add("我接受的任务");
-        list_title.add("我发布的任务");
+        list_title2 = new ArrayList<>();
+        list_title2.add("我接受的任务");
+        list_title2.add("我发布的任务");
 
         //设置TabLayout的模式,这里主要是用来显示tab展示的情况的
         //TabLayout.MODE_FIXED          各tab平分整个工具栏,如果不设置，则默认就是这个值
         //TabLayout.MODE_SCROLLABLE     适用于多tab的，也就是有滚动条的，一行显示不下这些tab可以用这个
         //                              当然了，你要是想做点特别的，像知乎里就使用的这种效果
-        tab_title.setTabMode(TabLayout.MODE_FIXED);
+        tab_title2.setTabMode(TabLayout.MODE_FIXED);
 
         //设置tablayout距离上下左右的距离
         //tab_title.setPadding(20,20,20,20);
 
         //为TabLayout添加tab名称
-        tab_title.addTab(tab_title.newTab().setText(list_title.get(0)));
-        tab_title.addTab(tab_title.newTab().setText(list_title.get(1)));
+        tab_title2.addTab(tab_title2.newTab().setText(list_title2.get(0)));
+        tab_title2.addTab(tab_title2.newTab().setText(list_title2.get(1)));
 
-        vAdapter = new viewAdapter(this,listViews,list_title,tabImg);
-        vp_pager.setAdapter(vAdapter);
+        vAdapter = new viewAdapter_history(this,listViews2,list_title2,tabImg);
+        vp_pager2.setAdapter(vAdapter);
 
         //将tabLayout与viewpager连起来
-        tab_title.setupWithViewPager(vp_pager);
+        tab_title2.setupWithViewPager(vp_pager2);
     }
 }
---
+++ app/src/main/java/com/example/sufehelperapp/MyActivity_History_Republish.java
@@ -0,0 +1,18 @@
+package com.example.sufehelperapp;
+
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyActivity_History_Republish extends AppCompatActivity {
+
+    @Nullable
+
+    public View onCreatView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_my_history_republish, container, false);
+        return view;
+    }
+}
