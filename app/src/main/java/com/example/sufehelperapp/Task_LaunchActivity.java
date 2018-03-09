bc01373012cccaf4fdc5848168ad999f841ae316
DaiSmallEast <970439131@qq.com>
2018-03-10 00:43:13 +0800

    add dxd_11 to drawable

--- app/src/main/java/com/example/sufehelperapp/Task_LaunchActivity.java
+++ app/src/main/java/com/example/sufehelperapp/Task_LaunchActivity.java
@@ -1,20 +1,42 @@
 package com.example.sufehelperapp;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.Locale;
 
 public class Task_LaunchActivity extends AppCompatActivity {
 
     public String subtaskType;
     public String area;
+
+    private Button mPickDate;
+    private int mYear;
+    private int mMonth;
+    private int mDay;
+    static final int DATE_DIALOG_ID = 0;
+    private int mHour;
+    private int mMinute;
+    static final int TIME_DIALOG_ID = 1;
+    private Button mPickTime;
 
     private user user;
 
@@ -50,7 +72,57 @@ public class Task_LaunchActivity extends AppCompatActivity {
         final TextView paymentView = (TextView) findViewById(R.id.launch_payment);
         final TextView descriptionView = (TextView) findViewById(R.id.launch_description);
 
+        mPickDate = (Button) findViewById(R.id.editDate);
+        mPickDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Task_LaunchActivity.this.showDialog(DATE_DIALOG_ID);
+            }
+        });
 
+        final Calendar c = Calendar.getInstance(Locale.CHINA);
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        mPickTime = (Button) findViewById(R.id.editTime);
+        mPickTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Task_LaunchActivity.this.showDialog(TIME_DIALOG_ID);
+            }
+        });
+
+        updateDisplay();
+        uptimeDisplay();
+
+        BottomNavigationView bottomNavigationItemView = (BottomNavigationView) findViewById(R.id.btn_navigation);
+        bottomNavigationItemView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId())
+                {
+                    case R.id.item_task:
+                        Intent intent1 = new Intent(Task_LaunchActivity.this, Task_HomeActivity.class);
+                        intent1.putExtra("user_now", user);
+                        startActivity(intent1);
+                        break;
+                    case R.id.item_explore:
+                        Intent intent3 = new Intent(Task_LaunchActivity.this, ExploreActivity.class);
+                        intent3.putExtra("user_now", user);
+                        startActivity(intent3);
+                        break;
+                    case R.id.item_my:
+                        Intent intent2 = new Intent(Task_LaunchActivity.this, My_HomeActivity.class);
+                        intent2.putExtra("user_now", user);
+                        startActivity(intent2);
+                        break;
+                }
+                return true;
+            }
+        });
         //detect spinner change
         subtaskView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
@@ -128,7 +200,50 @@ public class Task_LaunchActivity extends AppCompatActivity {
         });
 
     }
+    private void updateDisplay() {
+        mPickDate.setText(new StringBuffer()
+                //Month is 0 base so add 1
+                .append(mYear).append("-")
+                .append(mMonth + 1).append("-")
+                .append(mDay).append(" ")
 
+        );
+    }
+    private void uptimeDisplay() {
+        mPickTime.setText(new StringBuffer()
+                .append(mHour).append(":")
+                .append(mMinute).append(" "));
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        switch (id) {
+            case DATE_DIALOG_ID:
+                return new DatePickerDialog(this,DatePickerDialog.THEME_HOLO_LIGHT,mOnDateSetListener , mYear, mMonth, mDay);
+            case TIME_DIALOG_ID:
+                return new TimePickerDialog(this,TimePickerDialog.THEME_HOLO_LIGHT,onTimeSetListener,mHour,mMinute,true);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener mOnDateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    mYear = year;
+                    mMonth = monthOfYear;
+                    mDay = dayOfMonth;
+                    updateDisplay();
+                }
+            };
+    private TimePickerDialog.OnTimeSetListener onTimeSetListener =
+            new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                    mHour = i;
+                    mMinute = i1;
+                    uptimeDisplay();
+                }
+            };
 
     //未添加返回值
 
